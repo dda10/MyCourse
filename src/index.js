e20bbab0c8122049457a534e513007f8c112a897
dda <ddaduyanh123@gmail.com>
2022-03-17 15:58:19 +0700

    MVC model

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ const morgan = require("morgan");
 const app = express();
 const hbs = require("express-handlebars");
 const path = require("path");
+const port = 3000;
+const route = require("./routes");
+
+// Use middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // Static file
 app.use(express.static(path.join(__dirname, "public")));
@@ -20,13 +26,8 @@ app.engine(
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "resources/views"));
 
-app.get("/", (req, res) => {
-  res.render("home");
-});
-
-app.get("/news", (req, res) => {
-  res.render("news");
-});
+// Routes init
+route(app);
 
 // Port
-app.listen(3000);
+app.listen(port);
