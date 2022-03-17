954be2eb128070096834ffe401b40127c794f7fb
dda <ddaduyanh123@gmail.com>
2022-03-17 20:48:21 +0700

    prettier+husky+lint-staged

--- src/index.js
+++ src/index.js
@@ -1,30 +1,30 @@
-const express = require("express");
-const morgan = require("morgan");
+const express = require('express');
+const morgan = require('morgan');
 const app = express();
-const hbs = require("express-handlebars");
-const path = require("path");
+const hbs = require('express-handlebars');
+const path = require('path');
 const port = 3000;
-const route = require("./routes");
+const route = require('./routes');
 
 // Use middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Static file
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
-// HTTP logger
-app.use(morgan("combined"));
+// // HTTP logger
+// app.use(morgan("combined"));
 
 // Template engine
 app.engine(
-  "hbs",
+  'hbs',
   hbs.engine({
-    extname: ".hbs",
-  })
+    extname: '.hbs',
+  }),
 );
-app.set("view engine", "hbs");
-app.set("views", path.join(__dirname, "resources/views"));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
 
 // Routes init
 route(app);
